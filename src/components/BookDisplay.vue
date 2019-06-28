f7f66ad2af4a096360e18dee695697de9a64fdc4
newaeonweb <fernando@newaeonweb.com.br>
2019-06-28 15:32:11 -0300

    added book infos

--- src/components/BookDisplay.vue
+++ src/components/BookDisplay.vue
@@ -2,13 +2,19 @@
   <div class="flex flex-wrap my-12">
     <div class="w-full mb-8 md:mb-0 md:w-1/3">
       <img class="mx-auto medium-zoom-image" :src="'/images/' + image" :alt="title">
+      <div class="bg-gray-300 font-light h-6 text-center text-sm">
+        <span>Pages: {{ pages }} | Release: {{ release }}</span>
+      </div>
     </div>
     <div class="w-full md:w-2/3 px-3">
-      <h1 class="mb-2">{{ title }}</h1>
+      <h1 class="mb-2 font-sans">{{ title }}</h1>
       <p>{{ description }}</p>
-      <ul>
-        <li v-for="(feature, index) in features" :key="`feat-${index}`">{{ feature }}</li>
-      </ul>
+      <div class="mt-3">
+        <p class="border-b font-sans mb-1 pb-1">Learn:</p>
+        <ul class="list-disc pl-10 pb-4 text-gray-800">
+          <li v-for="(feature, index) in features" :key="`feat-${index}`">{{ feature }}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +25,8 @@ export default {
     image: String,
     title: String,
     description: String,
+    pages: String,
+    release: String,
     features: Array
   }
 };
--- src/pages/Books.vue
+++ src/pages/Books.vue
@@ -10,6 +10,8 @@
           v-for="book in books"
           :key="book.id"
           :image="book.image"
+          :pages="book.pages"
+          :release="book.release"
           :title="book.title"
           :description="book.description"
           :features="book.features"
@@ -39,47 +41,109 @@ export default {
       books: [
         {
           image: "fullstack-angular-laravel-docker.png",
+          pages: "420",
+          release: "2018",
           title:
             "Hands-On Full Stack Web Development with Angular 6 and Laravel 5",
           description:
-            "This book gives you practical knowledge of building modern full-stack web apps from scratch using Angular with a Laravel Restful back end. The book begins with a thorough introduction to Laravel and Angular and its core concepts like custom errors messages, components, routers, and Angular-cli, with each concept being explained first, and then put into practice in the case-study project. You will learn how to create secure web application with Angular and Laravel using token based authentication. Finally, you will learn all about progressive web applications and build and deploy a complete fullstack application using Docker and Docker-compose."
-          // features: ["Lindo", "MarAVILHOSO"]
+            "Become  fluent in both Front-end and Back-end web development with Docker, Angular and Laravel. By the end of this book, you'll gain a solid understanding of Angular 6 and Laravel 5.x.",
+          features: [
+            "Explore the core features of Angular.",
+            "Build an off-line-first application using service-worker.",
+            "Deal with token based authentication.",
+            "Deploy using Docker and Docker-compose.",
+            "How to use Laravel Resources and Eloquent ORM."
+          ]
         },
         {
           image: "node.JS-6.x-blueprints.jpg",
+          pages: "376",
+          release: "2016",
           title: "Node.js 6.x Blueprints",
           description:
-            "Node.js is the most popular framework for creating server-side applications today. Be it web, desktop, or mobile, Node.js comes to your rescue to create stunning real-time applications and Restfull API's. This book will teach you the MVC design pattern while developing a Twitter-like application using Express.js, Passport and MongoDB. You will learn to create database driven website as a consumer feedback app. How to using stream API to upload and manipulate images and video files. Once you're warmed up, we'll move on to more complex projects, such, an store locator using Geospatial Query, 2dspheres indexes with MongoDB and Google Maps, a real-time chat app with Socket.io, and a blog using Keystone.js and how to create Restfull API's with Loopback.io. Finally, we’ll explore frontend build processes using Node Package Manager, building Node applications with Docker, tests and continuous delivery. By the end of book, you will be comfortable working with Node.js applications and will know the best tools and frameworks for building highly scalable cloud applications."
+            "Create stunning web applications and Restful APIs from start to finish with Express, Mongoose, Satelizer, Loopback, MongoDB, and MySQL using this definitive guide will know the best tools and frameworks for building highly scalable cloud applications.",
+          features: [
+            "Explore MVC design pattern in Node.js.",
+            "Master the Express framework and build a complete application with a real database.",
+            "Build solid architectures by following test-driven development.",
+            "Build, Deploy, Docker, and continuous delivery.",
+            "MongoDB and Geospatial Query."
+          ]
         },
         {
           image: "angularJS-directives-cookbook.jpg",
+          pages: "206",
+          release: "2015",
           title: "AngularJS Directives Cookbook",
           description:
-            "This cookbook demonstrates how to take greater control over HTML elements on your web pages using directives – they ‘direct’ Angular’s HTML compiler to behave in the way you want it to. Packed with an extensive range of solutions and tips that AngularJS developers shouldn’t do without, you’ll find out how to make the most of directives. You’ll find recipes demonstrating how to build a number of different user interface components with directives and also how directives can simplify the way you work by creating reusable directives – by customizing them with Yeoman you can be confident that you’re application has the robust architecture that forms the bedrock of the best user experiences. You’ll also find recipes that will help you learn how to unit test directives, so you can be confident in the reliability and performance of your application. Extend the capabilities of AngularJS and build dynamic web applications by creating customized directives with this selection of more than 30 recipes. "
+            "Extend the capabilities of AngularJS and build dynamic web applications by creating customized directives with this selection of more than 30 recipes.",
+          features: [
+            "Build reusable directives for Large AngularJS applications",
+            "How to use jQuery events and manipulate the DOM.",
+            "Extend directives to use dynamic templates.",
+            "Write unit test for directives using the Karma.",
+            "Jasmine’s behavior-driven development framework."
+          ]
         },
         {
           image: "learning-single-page-web-application-development.jpg",
+          pages: "214",
+          release: "2014",
           title: "Learning Single-page Web Application Development",
           description:
-            "Learning Single-page Web Application Development is a journey through application development using the MEAN stack with JavaScript on the server side and client side, covering concepts such as RESTful, authentication and session on SPA. You will build large-scale applications. You will use a JavaScript environment with Node.js on the server, MongoDB as the database, and AngularJS on the frontend. Everything is integrated with the Bower dependency manager, Yeoman, and other modern tools to speed up your development process. You will deploy your application to the cloud and learn how to prepare your directory structure to scale and grow. Learning Single-page Web Application Development will give you an overview of AngularJS and RESTful APIs, and guide you in learning how to leverage single page development tools to create efficient and effective applications. "
+            "Build powerful and scalable single-page web applications using a full stack JavaScript environment with Node.js, MongoDB, AngularJS, and the Express framework.",
+          features: [
+            "Use the new features of Express 4.",
+            "Set up user authentication and store sessions.",
+            "Configure an AngularJS application using modules.",
+            "Event-driven development and the event loop.",
+            "Test AngularJS applications with Karma and Protractor."
+          ]
         },
         {
           image: "HTML5-responsive-table-design.jpg",
+          pages: "110",
+          release: "2013",
           title: "HTML5 Responsive Table Design",
           description:
-            "Instant HTML5 Responsive Table Design How-to will help you with powerful responsive design techniques that allow you to present your data in the best way depending on the device. This practical guide uses the latest web development techniques to construct and deconstruct tables. Taking you from basic table markup, this book will walk you through semantics tags, and the new features of CSS3 that will improve your tables to look pixel perfect on any device.Use practical recipes to understand responsive design techniques in relation to data tables. Advance your knowledge of tables and learn how to handle large volumes of data, apply filters to columns, hide less important data, and load content dynamically. No matter how big the data and how small the device, by the end of this book it won't matter. "
+            "Taking you from basic table markup, this book will walk you through semantics tags, and the new features of CSS3 that will improve your tables to look pixel perfect on any device. No matter how big the data and how small the device, by the end of this book it won't matter. ",
+          features: [
+            "Responsive design techniques in relation to data tables.",
+            "How to handle large volumes of data.",
+            "Apply filters to columns, hide less important data.",
+            "How to load content dynamically.",
+            "Converting tables into charts on mobile devices."
+          ]
         },
         {
           image: "node-projects-video.jpg",
+          pages: "2 Hours",
+          release: "2016",
           title: "Node Projects Video",
           description:
-            "This video takes a project-based approach, where every project will build up your Node.js skillset. By the end of the video, you will be able to create your own server-side applications using Node.js."
+            "A project based practical guide to create dynamic server-side applications.",
+          features: [
+            "Explore design patterns in Node.js.",
+            "Build solid architectures by following test-driven development.",
+            "Develop SPA using Node.js with AngularJS, Ember.js, and Backbone.js.",
+            "Create a real-time chat application with Socket.IO",
+            "Create three-four complete projects from scratch"
+          ]
         },
         {
           image: "nodejs-creating-applications-from-scratch.jpg",
+          pages: "4 Hours",
+          release: "2016",
           title: "Node.js: Creating Applications from Scratch",
           description:
-            "This course takes you from a newbie to a comfortable Node.js developer. It starts with the basics and in no time takes the project-specific approach. This course combines some of the best that Packt has to offer in one complete, curated package. It includes content from the following Packt products: Node.js 6.x Blueprints by Fernando Monteiro, Node.js Projects [Video] by Fernando Monteiro, The Complete Guide to Node.js [Video] by Joshua Johanan, Building Scalable Apps with Redis and Node.js by Joshua Johanan, Master the Express framework and build a complete site with a real database Deploying Node.js by Sandro Pasquali "
+            "This video takes a project-based approach, you will be able to create your own server-side applications using Node.js.",
+          features: [
+            "Explore the Node.js module system.",
+            "Build your custom middleware for Express.",
+            "Integrate Socke.io with Express.",
+            "Use Redis and Socket.io to make dynamic sites.",
+            "By the end of this course, you’ll be a skilled Node.js developer."
+          ]
         }
       ]
     };
