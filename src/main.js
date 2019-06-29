5d4d0230fcfdb0a28b4385adbd5bcdbb8b64980a
newaeonweb <fernando@newaeonweb.com.br>
2019-06-29 18:47:19 -0300

    added scrollTo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import DefaultLayout from '~/layouts/Default.vue';
 import 'prismjs/themes/prism-okaidia.css';
+import VueScrollTo from 'vue-scrollto';
 
 export default function(Vue, { head }) {
   Vue.component('Layout', DefaultLayout);
+
+  Vue.use(VueScrollTo, {
+    container: 'body',
+    duration: 500,
+    easing: 'ease',
+  });
 
   head.htmlAttrs = { lang: 'en', class: 'h-full' };
   head.bodyAttrs = { class: 'antialiased font-serif' };
